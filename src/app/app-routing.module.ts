258c0b8d3093ec9c2e93a64f14a31d7d4e1ab2cf
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-10-10 22:04:57 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { BemVindoComponent } from './bem-vindo/bem-vindo/bem-vindo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BemVindoComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule
+  ],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {
 
 }
